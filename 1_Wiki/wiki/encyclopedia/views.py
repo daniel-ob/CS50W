eec5db67102d577c3bd2d02ed129404734645fd2
daniel-ob <danobrey@gmail.com>
2021-06-10 16:09:59 +0200

    [1_Wiki] Implement Search functionality

--- 1_Wiki/wiki/encyclopedia/views.py
+++ 1_Wiki/wiki/encyclopedia/views.py
@@ -1,10 +1,27 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from . import util
 
 
 def index(request):
+    if request.method == "POST":
+        # coming from search query
+        title = request.POST["q"]
+        # if query matches an entry, redirect to that entry page
+        if util.get_entry(title):
+            return HttpResponseRedirect(reverse("entry", kwargs={"title": title}))
+
+        # list entries that have the query as substring
+        return render(request, "encyclopedia/index.html", {
+            "query": True,
+            "entries": [entry for entry in util.list_entries() if title.lower() in entry.lower()]
+        })
+
+    # list all entries
     return render(request, "encyclopedia/index.html", {
+        "query": False,
         "entries": util.list_entries()
     })
 
