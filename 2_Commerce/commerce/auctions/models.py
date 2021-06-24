893fb5f1993bce68d90c732092f91262f9d75c7d
daniel-ob <danobrey@gmail.com>
2021-06-24 15:09:33 +0200

    [2_Commerce] Add models: Category, Listing, Bid, Comment

--- 2_Commerce/commerce/auctions/models.py
+++ 2_Commerce/commerce/auctions/models.py
@@ -4,3 +4,44 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Listing(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name="listings", null=True, blank=True)
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=512, null=True, blank=True)
+    starting_bid_dollars = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    image_url = models.URLField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.title} ({self.user})"
+
+
+class Bid(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
+    amount_dollars = models.DecimalField(max_digits=8, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} offers {self.amount_dollars} for {self.listing}"
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
+    text = models.CharField(max_length=128)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} said: \'{self.text}\' on {self.listing}"
