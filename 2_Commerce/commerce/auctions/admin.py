464cc86b0e7037575034e3da33b31f04413df059
daniel-ob <danobrey@gmail.com>
2021-07-06 14:50:58 +0200

    [2_Commerce] Admin: Update Listings, Bids and Comments

--- 2_Commerce/commerce/auctions/admin.py
+++ 2_Commerce/commerce/auctions/admin.py
@@ -2,10 +2,23 @@ from django.contrib import admin
 
 from .models import User, Category, Listing, Bid, Comment, Watchlist
 
-# Register your models here.
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ("title", "user", "is_active", "creation_date")
+
+
+class BidAdmin(admin.ModelAdmin):
+    list_display = ("listing", "user", "amount_dollars", "date")
+    list_filter = ("listing", "user")
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user", "listing", "text", "date")
+
+
 admin.site.register(User)
 admin.site.register(Category)
-admin.site.register(Listing)
-admin.site.register(Bid)
-admin.site.register(Comment)
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(Bid, BidAdmin)
+admin.site.register(Comment, CommentAdmin)
 admin.site.register(Watchlist)
