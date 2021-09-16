317b0165c77ab7c52c0ca75fc1d03d22b6d29329
daniel-ob <danobrey@gmail.com>
2021-09-16 15:07:51 +0200

    [3_Mail] Admin interface: Add User and Email models

--- 3_Mail/mail/mail/admin.py
+++ 3_Mail/mail/mail/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import User, Email
+
+
 # Register your models here.
+class EmailAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "sender", "subject", "timestamp")
+    list_filter = ("user", "sender")
+
+
+admin.site.register(User)
+admin.site.register(Email, EmailAdmin)
