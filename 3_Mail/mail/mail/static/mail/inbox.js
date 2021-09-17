11f1a10c0491c9a4cbeee92110a357faf1a7400a
daniel-ob <danobrey@gmail.com>
2021-09-17 10:36:44 +0200

    [3_Mail] Load mailbox
    [3_Mail] Move h3 and email-container from .js to .html

--- 3_Mail/mail/mail/static/mail/inbox.js
+++ 3_Mail/mail/mail/static/mail/inbox.js
@@ -32,13 +32,66 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  document.querySelector('#inbox-name').innerHTML = `${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}`;
+
+  // Clear email-container
+  document.querySelector('#email-container').innerHTML = '';
+
+  // Get emails
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+    // Print emails
+    console.log(emails);
+
+    // Create one row <div> per email (using Bootstrap grid)
+    emails.forEach(email => {
+      const row = document.createElement('div');
+      const recipients = document.createElement('div');
+      const sender = document.createElement('div');
+      const subject = document.createElement('div');
+      const timestamp = document.createElement('div');
+
+      // Set styles
+      row.className = 'row border p-2';
+      if (email.read === false) {
+        row.className += ' bg-white';
+      } else {
+        row.className += ' bg-light';
+      }
+      recipients.className = 'col-sm-3 font-weight-bold text-break';
+      sender.className = 'col-sm-3 font-weight-bold text-break';
+      subject.className = 'col-sm-6';
+      timestamp.className = 'col-sm-3 text-right';
+
+      // Set email details
+      recipients.innerHTML = 'To: ' + email.recipients;
+      sender.innerHTML = email.sender;
+      subject.innerHTML = email.subject;
+      timestamp.innerHTML = email.timestamp;
+
+      // Show sender or recipients address depending on mailbox
+      if (mailbox === 'sent') {
+        row.append(recipients)
+      } else {
+        row.append(sender);
+      }
+      row.append(subject);
+      row.append(timestamp);
+
+      // Add email to container
+      document.querySelector('#email-container').append(row);
+
+      // Open the email when user clicks on it
+      row.addEventListener('click', () => view_email(email.id));
+    });
+  })
 }
 
 function send_email() {
