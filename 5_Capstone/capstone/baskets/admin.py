ed958961c1fdd48dbd41da30e35b68c44d5acf34
daniel-ob <danobrey@gmail.com>
2021-10-20 15:41:30 +0200

    [5_Capstone] Models: Add Delivery, UserOrder, UserOrderItem

--- 5_Capstone/capstone/baskets/admin.py
+++ 5_Capstone/capstone/baskets/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User, Producer, Product
+from .models import User, Producer, Product, Delivery, UserOrder, UserOrderItem
 
 
 class ProductInline(admin.TabularInline):
@@ -11,5 +11,22 @@ class ProducerAdmin(admin.ModelAdmin):
     inlines = [ProductInline]
 
 
+class DeliveryAdmin(admin.ModelAdmin):
+    list_display = ("date", )
+
+
+class UserOrderItemInline(admin.TabularInline):
+    model = UserOrderItem
+
+
+class UserOrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "delivery", "amount", "creation_date")
+    list_filter = ("user", "delivery")
+    readonly_fields = ["amount", "creation_date"]
+    inlines = [UserOrderItemInline]
+
+
 admin.site.register(User)
 admin.site.register(Producer, ProducerAdmin)
+admin.site.register(Delivery, DeliveryAdmin)
+admin.site.register(UserOrder, UserOrderAdmin)
--- 5_Capstone/capstone/baskets/models.py
+++ 5_Capstone/capstone/baskets/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import RegexValidator
+from django.core.validators import RegexValidator, MinValueValidator
 from django.db import models
 
 # FR phone numbers regex
@@ -34,3 +34,31 @@ class Product(models.Model):
     def __str__(self):
         return f"{self.name}"
 
+
+class Delivery(models.Model):
+    date = models.DateField(blank=False, unique=True)
+    products = models.ManyToManyField(Product, related_name="deliveries")
+    message = models.CharField(blank=True, max_length=128)
+
+    def __str__(self):
+        return f"{self.date}"
+
+
+class UserOrder(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name="orders")
+    delivery = models.ForeignKey(Delivery, on_delete=models.PROTECT, related_name="user_orders")
+    creation_date = models.DateTimeField(auto_now_add=True)
+    amount = models.DecimalField(default=0.00, max_digits=8, decimal_places=2, editable=False)
+    message = models.CharField(blank=True, max_length=128)
+
+    def __str__(self):
+        return f"From {self.user} for {self.delivery.date}"
+
+
+class UserOrderItem(models.Model):
+    user_order = models.ForeignKey(UserOrder, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name="user_order_items")
+    quantity = models.PositiveIntegerField(null=False, validators=[MinValueValidator(1)])
+
+    def __str__(self):
+        return f"{self.user_order}: {self.quantity} x {self.product}"
