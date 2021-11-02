5543eff86f6a00297abc0edf5af2cdf21d3642e6
daniel-ob <danobrey@gmail.com>
2021-11-02 11:59:37 +0100

    [5_Capstone] Add API Tests

--- 5_Capstone/capstone/baskets/tests.py
+++ 5_Capstone/capstone/baskets/tests.py
@@ -1,11 +1,14 @@
 import datetime
+import json
 
 from django.test import Client, TestCase
+from django.urls import reverse
 
 from .models import User, Producer, Product, Delivery, Order, OrderItem
 
 
 class BasketsTestCase(TestCase):
+    """Define shared setUp() for Baskets app tests"""
 
     def setUp(self):
         # Create users
@@ -22,15 +25,15 @@ class BasketsTestCase(TestCase):
         self.product3 = Product.objects.create(producer=self.producer2, name="product3", unit_price=1.15)
 
         # Create deliveries
-        self.d1 = Delivery.objects.create(date=datetime.date(2021, 11, 23))
+        self.d1 = Delivery.objects.create(date=datetime.date(2021, 11, 23), message="delivery 1")
         self.d1.products.set([self.product1, self.product2])
 
-        self.d2 = Delivery.objects.create(date=datetime.date(2021, 12, 14))
+        self.d2 = Delivery.objects.create(date=datetime.date(2021, 12, 14), message="delivery 2")
         self.d2.products.set([self.product1, self.product3])
 
         # Create orders
-        self.o1 = Order.objects.create(user=self.u1, delivery=self.d1)
-        self.o2 = Order.objects.create(user=self.u2, delivery=self.d2)
+        self.o1 = Order.objects.create(user=self.u1, delivery=self.d1, message="order 1")
+        self.o2 = Order.objects.create(user=self.u2, delivery=self.d2, message="order 2")
 
         # Create order items
         self.oi1 = OrderItem.objects.create(order=self.o1, product=self.product1, quantity=4)
@@ -41,6 +44,9 @@ class BasketsTestCase(TestCase):
 
         # Create test client
         self.c = Client()
+
+
+class ModelsTestCase(BasketsTestCase):
 
     def test_producer_products_count(self):
         self.assertEqual(self.producer1.products.count(), 2)
@@ -113,3 +119,224 @@ class BasketsTestCase(TestCase):
         """Check that order item is invalid if quantity is not greater than 0"""
 
         self.assertEqual(self.oi4.is_valid(), False)
+
+
+class APITestCase(BasketsTestCase):
+    def test_order_creation(self):
+        """Check that user1 can create an order through API"""
+
+        # log-in user1
+        self.c.force_login(self.u1)
+
+        user1_orders_count_initial = self.u1.orders.count()
+
+        order_json = {
+            "delivery_id": self.d2.id,
+            "items": [
+                {
+                    "product_id": self.product1.id,
+                    "quantity": 1
+                },
+                {
+                   "product_id": self.product3.id,
+                   "quantity": 2
+                }
+            ],
+            "message": "One product1 and two product3"
+        }
+        response = self.c.post(reverse("create_order"), data=json.dumps(order_json), content_type="application/json")
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(float(response.json()["amount"]), 2.80)
+        self.assertEqual(response.json()["url"], reverse("order", args=[Order.objects.last().id]))
+
+        self.assertEqual(self.u1.orders.count(), user1_orders_count_initial + 1)
+
+        new_order = Order.objects.last()
+        self.assertEqual(new_order.delivery, self.d2)
+        self.assertEqual(new_order.message, "One product1 and two product3")
+        self.assertEqual(new_order.items.count(), 2)
+
+    def test_order_creation_not_authenticated(self):
+        """Check that a non authenticated user gets an "Unauthorized" error when trying to create order through API"""
+
+        orders_count_initial = Order.objects.count()
+
+        order_json = {
+            "delivery_id": self.d2.id,
+            "items": [
+                {
+                    "product_id": self.product1.id,
+                    "quantity": 1
+                }
+            ]
+        }
+        response = self.c.post(reverse("create_order"), data=json.dumps(order_json), content_type="application/json")
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(Order.objects.count(), orders_count_initial)
+
+    def test_order_creation_invalid_second_order_for_delivery(self):
+        """Check that user1 receives an error 400 when trying to create a second order for a given delivery"""
+
+        self.c.force_login(self.u1)
+        user1_orders_count_initial = self.u1.orders.count()
+
+        # user already has an order for d1
+        order_json = {
+            "delivery_id": self.d1.id
+        }
+        response = self.c.post(reverse("create_order"), data=json.dumps(order_json), content_type="application/json")
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(self.u1.orders.count(), user1_orders_count_initial)
+
+    def test_order_creation_invalid_product(self):
+        """Check that user1 receives an error 400 when creating an order with a product not available in delivery"""
+
+        self.c.force_login(self.u1)
+        user1_orders_count_initial = self.u1.orders.count()
+
+        # product2 is not available in d2
+        order_json = {
+            "delivery_id": self.d2.id,
+            "items": [
+                {
+                    "product_id": self.product1.id,
+                    "quantity": 1
+                },
+                {
+                   "product_id": self.product2.id,
+                   "quantity": 1
+                }
+            ],
+        }
+        response = self.c.post(reverse("create_order"), data=json.dumps(order_json), content_type="application/json")
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(self.u1.orders.count(), user1_orders_count_initial)
+
+    def test_order_get(self):
+        """Check that user1 can retrieve one of its orders"""
+
+        self.c.force_login(self.u1)
+
+        o1_expected_json = {
+            'delivery_id': 1,
+            'items': [
+                {
+                    'product': {
+                        'id': 1,
+                        'name': 'product1',
+                        'unit_price': '0.50'
+                    },
+                    'quantity': 4,
+                    'amount': '2.00'
+                },
+                {
+                    'product': {
+                        'id': 2,
+                        'name': 'product2',
+                        'unit_price': '1.00'
+                    },
+                    'quantity': 1,
+                    'amount': '1.00'
+                }
+            ],
+            'amount': '3.00',
+            'message': 'order 1'
+        }
+
+        response = self.c.get(reverse("order", args=[self.o1.id]))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), o1_expected_json)
+
+    def test_order_get_invalid_user(self):
+        """Check that user1 gets a 'Forbidden' error when trying to retrieve an user2 order"""
+
+        self.c.force_login(self.u1)
+        response = self.c.get(reverse("order", args=[self.u2.orders.last().id]))
+        self.assertEqual(response.status_code, 403)
+
+    def test_order_update(self):
+        """Check that user1 can update an order through API"""
+
+        # log-in user1
+        self.c.force_login(self.u1)
+
+        # Create an order for this test
+        order = Order.objects.create(user=self.u1, delivery=self.d2, message="test order")
+        order_item1 = OrderItem.objects.create(order=order, product=self.product1, quantity=1)
+        order_item2 = OrderItem.objects.create(order=order, product=self.product3, quantity=3)
+
+        updated_order_json = {
+            "items": [
+                {
+                    "product_id": self.product3.id,
+                    "quantity": 2
+                }
+            ],
+            "message": "order updated"
+        }
+        response = self.c.put(reverse("order", args=[self.u1.orders.last().id]),
+                              data=json.dumps(updated_order_json),
+                              content_type="application/json")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(float(response.json()['amount']), 2.30)
+
+        updated_order = self.u1.orders.last()
+        self.assertEqual(updated_order.message, "order updated")
+        self.assertEqual(updated_order.items.count(), 1)
+
+    def test_order_delete(self):
+        """Check that user1 can delete one of its orders, including all of its items, through API"""
+
+        self.c.force_login(self.u1)
+
+        # Create an order for this test
+        order = Order.objects.create(user=self.u1, delivery=self.d2)
+        order_item1 = OrderItem.objects.create(order=order, product=self.product1, quantity=1)
+        order_item2 = OrderItem.objects.create(order=order, product=self.product3, quantity=3)
+
+        response = self.c.delete(reverse("order", args=[order.id]))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertNotIn(order, self.u1.orders.all())
+        self.assertNotIn(order_item1, OrderItem.objects.all())
+        self.assertNotIn(order_item2, OrderItem.objects.all())
+
+    def test_order_delete_not_authenticated(self):
+        """Check that a not authenticated user gets an "Unauthorized" error when trying to delete order through API"""
+
+        response = self.c.delete(reverse("order", args=[self.o1.id]))
+
+        self.assertEqual(response.status_code, 401)
+        self.assertIn(self.o1, Order.objects.all())
+
+    def test_delivery_get(self):
+        """Check that a delivery can be retrieved through API"""
+
+        d1_expected_json = {
+            "date": "2021-11-23",
+            "order_deadline": "2021-11-19",
+            "products": [
+                {
+                    "id": 1,
+                    "name": "product1",
+                    "unit_price": "0.50"
+                },
+                {
+                    "id": 2,
+                    "name": "product2",
+                    "unit_price": "1.00"
+                }
+            ],
+            "message": "delivery 1"
+        }
+
+        response = self.c.get(reverse("delivery", args=[self.d1.id]))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), d1_expected_json)
