d8add43f8048a9380c80b173a0fdb9ac39f97698
daniel-ob <danobrey@gmail.com>
2021-11-18 11:32:54 +0100

    [5_Capstone] Password reset: use custom templates

--- 5_Capstone/capstone/baskets/forms.py
+++ 5_Capstone/capstone/baskets/forms.py
@@ -1,4 +1,7 @@
-from django.forms import ModelForm, TextInput
+from django.contrib.auth import password_validation
+from django.contrib.auth.forms import PasswordResetForm, SetPasswordForm
+from django.forms import ModelForm, TextInput, EmailField, EmailInput, CharField, PasswordInput
+from django.utils.translation import gettext_lazy as _
 
 from .models import User
 
@@ -20,3 +23,34 @@ class NewUserForm(ModelForm):
             "address":      TextInput(attrs={"class": "form-control", "placeholder": "Address"}),
             "password":     TextInput(attrs={"class": "form-control", "type": "password", "placeholder": "Password"})
         }
+
+
+class BasketsPasswordResetForm(PasswordResetForm):
+    """Extends default PasswordResetForm to customize email widget attributes"""
+
+    email = EmailField(
+        label="",
+        max_length=254,
+        widget=EmailInput(attrs={"class": "form-control",
+                                 "placeholder": _("Email"),
+                                 "autocomplete": "email",
+                                 "autofocus": ""})
+    )
+
+
+class BasketsSetPasswordForm(SetPasswordForm):
+    """Extends default SetPasswordForm to customize passwords widgets attributes"""
+
+    new_password1 = CharField(
+        widget=PasswordInput(attrs={"class": "form-control",
+                                    "placeholder": _("New password"),
+                                    "autocomplete": "new-password"}),
+        strip=False,
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    new_password2 = CharField(
+        strip=False,
+        widget=PasswordInput(attrs={"class": "form-control",
+                                    "placeholder": _("New password confirmation"),
+                                    "autocomplete": "new-password"}),
+    )
--- 5_Capstone/capstone/baskets/urls.py
+++ 5_Capstone/capstone/baskets/urls.py
@@ -2,17 +2,33 @@ from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
+from .forms import BasketsPasswordResetForm, BasketsSetPasswordForm
 
 urlpatterns = [
-    # Accounts
     path("", views.index, name="index"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("register/", views.register, name="register"),
-    path("password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"),
-    path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+
+    # Password reset. Use default auth_views with custom templates and forms
+    path("password_reset/",
+         auth_views.PasswordResetView.as_view(
+             template_name="baskets/password/password_reset.html",
+             form_class=BasketsPasswordResetForm),
+         name="password_reset"),
+    path("password_reset/done/",
+         auth_views.PasswordResetDoneView.as_view(
+             template_name="baskets/password/password_reset_done.html"),
+         name="password_reset_done"),
+    path("reset/<uidb64>/<token>/",
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name="baskets/password/password_reset_confirm.html",
+             form_class=BasketsSetPasswordForm),
+         name="password_reset_confirm"),
+    path("reset/done/",
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name="baskets/password/password_reset_complete.html"),
+         name="password_reset_complete"),
 
     # API Routes
     path("orders", views.create_order, name="create_order"),
