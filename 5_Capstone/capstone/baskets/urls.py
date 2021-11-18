7566b29113eca4033d332a3da833ebc061a02c51
daniel-ob <danobrey@gmail.com>
2021-11-18 10:50:05 +0100

    [5_Capstone] Add 'password reset' flow, default templates

--- 5_Capstone/capstone/baskets/urls.py
+++ 5_Capstone/capstone/baskets/urls.py
@@ -1,12 +1,18 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
+    # Accounts
     path("", views.index, name="index"),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("register/", views.register, name="register"),
+    path("password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"),
+    path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 
     # API Routes
     path("orders", views.create_order, name="create_order"),
